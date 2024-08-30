54e0b12a21d378eb10dc476e33ac137b6b54c967
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-08-30 22:17:23 +0200

    add tests for legal targets on empty board

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require_relative '../lib/knight'
 
 describe Knight do
@@ -22,5 +24,40 @@ describe Knight do
   end
 
   describe '#update_legal_targets' do
+    before do
+      let(:board) { double('board') }
+      allow(board).to receive(:state).and_return(Array.new(8) { Array.new(8) })
+    end
+
+    context 'when the knight is on an empty board' do
+      it 'can make all 8 moves from center square e4 (coords 4,3)' do
+        knight.move([4, 3])
+        knight.update_legal_targets(board)
+        result = [[5, 5], [5, 1], [3, 5], [3, 1], [6, 4], [6, 2], [2, 4], [2, 2]]
+        expect(knight.legal_targets.sort).to eq(result.sort)
+      end
+
+      it 'can make 2 moves in positive direction from corner square a1 (coords 0,0)' do
+        knight.move([0, 0])
+        knight.update_legal_targets(board)
+        result = [[1, 2], [2, 1]]
+        expect(knight.legal_targets.sort).to eq(result.sort)
+      end
+
+      it 'can make 2 moves in negative direction from corner square h8 (coords 7,7)' do
+        knight.move([7, 7])
+        knight.update_legal_targets(board)
+        result = [[5, 6], [6, 5]]
+        expect(knight.legal_targets.sort).to eq(result.sort)
+      end
+
+      it 'can make 4 moves from edge square a5 (coords 0,4)' do
+        knight.move([0, 4])
+        knight.update_legal_targets(board)
+        result = [[1, 6], [1, 2], [2, 5], [2, 3]]
+        expect(knight.legal_targets.sort).to eq(result.sort)
+      end
+    end
   end
 end
+# rubocop:enable Metrics/BlockLength
