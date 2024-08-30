ee4fa3bf9e4bfac45ef023d06a87b793cc967ee1
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-08-30 22:27:14 +0200

    make update legal targets work on empty board

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,4 +9,16 @@ class Knight < Piece
     super
     @relative_moves = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
   end
+
+  def update_legal_targets(board)
+    result = []
+    @relative_moves.each do |move|
+      x = position[0] + move[0]
+      y = position[1] + move[1]
+      next unless x.between?(0, 7) && y.between?(0, 7)
+
+      result.append([x, y])
+    end
+    @legal_targets = result
+  end
 end
