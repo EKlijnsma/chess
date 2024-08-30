b504c9323872f42643fe4dc144883bfa4b049e24
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-08-30 23:14:32 +0200

    implement additional step for update_legal_targets
    now the method also excludes squares that are occupied by own pieces
    of the same color

--- lib/knight.rb
+++ lib/knight.rb
@@ -11,14 +11,28 @@ class Knight < Piece
   end
 
   def update_legal_targets(board)
+    targets = generate_on_board_targets
+    @legal_targets = exclude_own_piece_positions(targets, board)
+  end
+
+  private
+
+  def generate_on_board_targets
     result = []
     @relative_moves.each do |move|
       x = position[0] + move[0]
       y = position[1] + move[1]
       next unless x.between?(0, 7) && y.between?(0, 7)
 
-      result.append([x, y])
+      result << [x, y]
     end
-    @legal_targets = result
+    result
+  end
+
+  def exclude_own_piece_positions(targets, board)
+    targets.select do |target|
+      target_state = board.state[target[0]][target[1]]
+      target_state.nil? || target_state.color != color
+    end
   end
 end
