38646637b7a835c5700ae5fbf5344a9833137fd7
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-09-03 13:25:41 +0200

    implement method to account for pieces in pawn move

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -9,4 +9,30 @@ class Pawn < Piece
     super
     @relative_moves = { up: [[0, 1], [0, 2]], up_right: [[1, 1]], up_left: [[-1, 1]] }
   end
+
+  def account_for_pieces(targets, board)
+    moves = {}
+    targets.each do |direction, squares|
+      # Get the actual state (content) of the board at all squares
+      squares_states = squares.map { |square| board.state[square[0]][square[1]] }
+
+      # make sure every direction exists in the moves output
+      moves[direction] = []
+
+      # if in diagonal direction, there is a piece and it is not the same color as self, add the squares to legal moves
+      if %i[up_left up_right].include?(direction) && (squares_states[0] && squares_states[0].color != color)
+        moves[direction] = squares
+
+      elsif direction == :up
+        # if a piece sits at the next square, move on to the next direction
+        next if squares_states[0]
+
+        # if it doesnt, add this square
+        moves[direction].push(squares[0])
+        # add the 2-step move only if current position is on the 1st rank and no piece sits at the target square
+        moves[direction].push(squares[1]) if position[1] == 1 && squares_states[1].nil?
+      end
+    end
+    moves
+  end
 end
